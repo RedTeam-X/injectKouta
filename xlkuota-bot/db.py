b51f81eb3955e39bc200b72a44400b8abbe67893
RedTeam-X <exploitation.redteam@gmail.com>
2026-01-27 06:48:11 +0700

    Update db.py

--- xlkuota-bot/db.py
+++ xlkuota-bot/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-# SQLite lokal (Railway juga bisa pakai ini untuk awal)
 engine = create_engine("sqlite:///xlkuota.db")
 Base.metadata.create_all(engine)
 
