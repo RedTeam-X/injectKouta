8eec71f7d2365096d0a7362641d7890d39502812
RedTeam-X <exploitation.redteam@gmail.com>
2026-01-27 07:34:34 +0700

    Update config.py

--- xlkuota-bot/config.py
+++ xlkuota-bot/config.py
@@ -1,9 +1,9 @@
-BOT_TOKEN = "8544774444:AAH13JEgpbPZfBEdgO6KProb9sSENAWCxFA"
+BOT_TOKEN = "8429800345:AAG1msHIiec1gTtYq09H7JWTHkKMZf0sM9I"
 
 # Ganti dengan Telegram ID admin (integer, bukan username)
 ADMIN_CHAT_ID = 7111874161 
 
-MIN_TOPUP = 20000
+MIN_TOPUP = 20.000
 
 # Path lokal gambar QRIS (disimpan di folder assets/)
 QRIS_IMAGE_PATH = "assets/qris.png"
