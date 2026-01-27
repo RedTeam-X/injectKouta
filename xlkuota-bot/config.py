3b49a926ba9f85a2b4168cf150207d1d1836de1e
RedTeam-X <exploitation.redteam@gmail.com>
2026-01-27 07:36:32 +0700

    Update config.py

--- xlkuota-bot/config.py
+++ xlkuota-bot/config.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = "8429800345:AAG1msHIiec1gTtYq09H7JWTHkKMZf0sM9I"
 
 # Ganti dengan Telegram ID admin (integer, bukan username)
-ADMIN_CHAT_ID = 7111874161 
+ADMIN_CHAT_ID = "7111874161" 
 
 MIN_TOPUP = 20.000
 
