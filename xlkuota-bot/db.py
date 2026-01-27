ec52704153bae5cbe368f9f49420c9f6201300d2
RedTeam-X <exploitation.redteam@gmail.com>
2026-01-27 07:24:25 +0700

    Update db.py

--- xlkuota-bot/db.py
+++ xlkuota-bot/db.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(engine)
 
 SessionLocal = sessionmaker(bind=engine)
