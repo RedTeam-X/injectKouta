958c4b5524b6bfc0f7fbf24792b65410c21c53c2
RedTeam-X <exploitation.redteam@gmail.com>
2026-01-30 09:26:45 +0700

    Update db.py

--- xlkuota-bot/db.py
+++ xlkuota-bot/db.py
@@ -2,7 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 from models import Base
+from sqlalchemy import text
+from sqlalchemy.exc import ProgrammingError
 
+def ensure_kategori_column_exists():
+    with engine.connect() as conn:
+        try:
+            conn.execute(text("ALTER TABLE ppob_items ADD COLUMN kategori VARCHAR"))
+        except ProgrammingError as e:
+            if "already exists" in str(e):
+                pass  # kolom sudah ada, aman
+            else:
+                raise
+              
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(engine)
 
